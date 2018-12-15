53c4abe526bd677cbc4247f7138d63d87f02b683
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2018-12-16 00:32:05 +0800

    Create train_station_Sinfo.php

---
+++ train_station management/train_station_Sinfo.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sold info</title>
+</head>
+<body>
+	<h1>Sold information</h1>
+	<?php
+		$db =new mysqli('localhost','logging','log','train');
+		if(mysqli_connect_errno())
+		{
+			echo "<p>Error: Could not connect to database.<br />
+			Please try again later.<br />";
+			exit;
+		}
+		$query="select * from Tinfo";
+		$stmt=$db->prepare($query);
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($number,$start,$terminal,$launchtime,$arrivetime,$price,$therest,$total);
+		if($stmt->num_rows==0)
+		{
+			echo "Sorry, there are some errors";
+			exit;
+		}
+		else
+		{
+			while($stmt->fetch())
+			{
+				echo "<p><strong>number: ".$number."</strong>";
+				echo "<br />rest: ".$therest;
+				echo "<br />sold: ".($total-$therest)."</p>";
+			}
+		}
+		$stmt->free_result(); //释放结果集
+		$db->close(); //关闭数据库连接
+	?>
+</body>
+</html>
